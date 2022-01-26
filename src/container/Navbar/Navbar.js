a571f2d36df2bb7fcbfb9f67e84988c70944f874
Anmol Singh <anmolsbhinder@gmail.com>
2022-01-27 01:07:39 +0530

    hamburger

--- src/container/Navbar/Navbar.js
+++ src/container/Navbar/Navbar.js
@@ -3,6 +3,11 @@ import "./Navbar.css";
 import logoLight from "./logo variation-03.png";
 import logoDark from "./logo variation-01.png";
 import Button from "../../node_modules/@mui/material/Button/Button";
+import Icon from "../../node_modules/@mui/icons-material/Menu";
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+></link>;
 
 export default function Navbar() {
   const [navbar, setNavbar] = useState(false);
@@ -20,6 +25,17 @@ export default function Navbar() {
 
   window.addEventListener("scroll", changeBackground);
 
+  function myFunction() {
+    console.log("test123");
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav_links" || x.className === "nav_links act") {
+      x.className += " responsive";
+    } else {
+      if (x.className === "nav_links responsive") x.className = "nav_links";
+      else x.className = "nav_links act";
+    }
+  }
+
   return (
     <nav
       className={navbar ? "navbar active fixed-top" : "navbar fixed-top"}
@@ -33,7 +49,7 @@ export default function Navbar() {
         src={navbar ? logoDark : logoLight}
         alt="store logo"
       />
-      <div className={navbar ? "nav_links act" : "nav_links"}>
+      <div className={navbar ? "nav_links act" : "nav_links"} id="myTopnav">
         <li className={navbar ? "nav_menu act" : "nav_menu"}>About</li>
         <li className={navbar ? "nav_menu act" : "nav_menu"}>Projects</li>
         <li className={navbar ? "nav_menu act" : "nav_menu"}>Community</li>
@@ -44,8 +60,30 @@ export default function Navbar() {
         >
           Contact Us
         </Button>
-        <li href="javascript:void(0);" class="icon" onclick="myFunction()">
-          <i class="fa fa-bars"></i>
+
+        <li
+          href="javascript:void(0);"
+          class="icon"
+          onClick={() => {
+            var x = document.getElementById("myTopnav");
+            if (
+              x.className === "nav_links" ||
+              x.className === "nav_links act"
+            ) {
+              console.log("test1");
+              x.className += " responsive";
+            } else {
+              if (x.className === "nav_links responsive") {
+                x.className = "nav_links";
+                console.log("test2");
+              } else {
+                x.className = "nav_links act";
+                console.log("test3");
+              }
+            }
+          }}
+        >
+          click
         </li>
       </div>
     </nav>
