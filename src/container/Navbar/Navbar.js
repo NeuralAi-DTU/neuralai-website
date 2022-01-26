eba2105d5811c6ad72ee59065410440ea6e8c782
Anmol Singh <anmolsbhinder@gmail.com>
2022-01-26 22:32:57 +0530

    enhanced navbar

--- src/container/Navbar/Navbar.js
+++ src/container/Navbar/Navbar.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import "./Navbar.css";
 import logo from "./logo192.png";
+import Button from "../../node_modules/@mui/material/Button/Button";
 
 export default function Navbar() {
   const [navbar, setNavbar] = useState(false);
@@ -28,12 +29,20 @@ export default function Navbar() {
     >
       <img className="logo1" src={logo}></img>
       <div className={navbar ? "nav_links act" : "nav_links"}>
-        <h4 className={navbar ? "nav_menu act" : "nav_menu"}>About</h4>
-        <h4 className={navbar ? "nav_menu act" : "nav_menu"}>Projects</h4>
-        <h4 className={navbar ? "nav_menu act" : "nav_menu"}>Community</h4>
-        <h4 className={navbar ? "nav_menu act" : "nav_menu"}>Team</h4>
+        <li className={navbar ? "nav_menu act" : "nav_menu"}>About</li>
+        <li className={navbar ? "nav_menu act" : "nav_menu"}>Projects</li>
+        <li className={navbar ? "nav_menu act" : "nav_menu"}>Community</li>
+        <li className={navbar ? "nav_menu act" : "nav_menu"}>Team</li>
+        <Button
+          variant="outlined"
+          className={navbar ? "nav_button act" : "nav_button"}
+        >
+          Contact Us
+        </Button>
+        <li href="javascript:void(0);" class="icon" onclick="myFunction()">
+          <i class="fa fa-bars"></i>
+        </li>
       </div>
-      <img className="logo1" src={logo}></img>
     </nav>
   );
 }
